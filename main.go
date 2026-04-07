8f5107ac05fc88399be1749904c36576ec9cedc6
shaunak <shaunakondare44@gmail.com>
2026-04-07 14:05:06 +0530

    changed go mod

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	// Standard Prometheus metrics endpoint
+	http.Handle("/metrics", promhttp.Handler())
+
+	// Your static file server
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
